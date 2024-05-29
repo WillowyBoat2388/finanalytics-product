111160d0fc893b73bc8f659fc1c614f3b3767740
Onidajo_anu <onidajo99@gmail.com>
2024-05-29 10:03:58 +0000

    -M Modified bash script

--- finnhub-batch-stock-pipeline/apps/bash.sh
+++ finnhub-batch-stock-pipeline/apps/bash.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
+# Function to detect the terminal emulator
+terminal_window() {
+
+# Check for common terminal emulators
+    if [ "$GNOME_TERMINAL_SCREEN" ]; then
+        echo "terminal found, script running begins"
+    elif [ "$KONSOLE_DBUS_SESSION" ]; then
+        echo "terminal found, script running begins"
+    elif [ "$TERM" == "xterm-256color" ]; then
+        echo "terminal found, script running begins"
+    else
+        echo "No compatible terminal emulator found!"
+        exit 1
+    fi
+}
+
 # Function to detect the terminal emulator and open a new terminal
 open_new_terminal() {
     local command="$1"
     
     # Check for common terminal emulators
-    if command -v gnome-terminal &> /dev/null; then
+    if [ "$GNOME_TERMINAL_SCREEN" ]; then
         gnome-terminal -- bash -c "$command; exec bash"
-    elif command -v konsole &> /dev/null; then
+    elif [ "$KONSOLE_DBUS_SESSION" ]; then
         konsole --noclose -e bash -c "$command; exec bash"
-    elif command -v xterm &> /dev/null; then
+    elif [ "$TERM" == "xterm-256color" ]; then
         xterm -hold -e "$command; bash"
     else
         echo "No compatible terminal emulator found!"
         exit 1
     fi
 }
+
+terminal_window
 
 SCRIPT_DIR=$(dirname "$BASH_SOURCE")
 
@@ -27,27 +45,27 @@ chmod +x $SCRIPT_DIR/mc
 # ./mc alias set minio http://127.0.0.1:9000 minioadmin minioadmin
 
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install terraform
+apt update && apt install terraform
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
-&& sudo chmod +x /usr/local/bin/docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
+# && sudo chmod +x /usr/local/bin/docker-compose
 
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+# sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 minio_startcommand = "$SCRIPT_DIR/minio server ../data --console-address :9001"
 
 # xterm -hold -e "$minio_startcommand; bash"
 
 open_new_terminal "$minio_startcommand"
-$SCRIPT_DIR/mc alias set minio http://localhost:9000 minioadmin minioadmin
+$SCRIPT_DIR/mc alias set minio http://127.0.0.1:9000 minioadmin minioadmin
 $SCRIPT_DIR/mc admin info minio > minio.txt
 $SCRIPT_DIR/mc mb minio/dagster-api
 
-minikube_startcommand = "minikube start"
+minikube_startcommand = "minikube start --force"
 
 open_new_terminal "minikube_startcommand"
 
@@ -65,6 +83,8 @@ docker build -t fnhb-btch-stck-ppln .
 # minikube image load <image name>
 
 cd terraform
+
+# terraform plan
 
 # terraform import kubernetes_secret.pipeline-secrets 
 
