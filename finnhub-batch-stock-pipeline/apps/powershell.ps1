0603574146fc18d542e7a925903555b5a00e0a23
Prince Fikayo <onidajo99@gmail.com>
2024-05-29 13:10:01 +0000

    Removed powershell script

--- finnhub-batch-stock-pipeline/apps/powershell.ps1
+++
@@ -1,8 +0,0 @@
-& minikube -p minikube docker-env --shell powershell | Invoke-Expression
-minikube docker-env | Invoke-Expression # PowerShell
-
-docker build -t <image-tag> <my-docker-image>
-
-minikube cache add <my-docker-image>
-
-minikube image load <image name>
