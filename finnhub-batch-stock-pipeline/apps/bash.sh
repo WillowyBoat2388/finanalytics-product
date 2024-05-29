1b064aa986b883938ed48074b52c4796410d683d
Onidajo_anu <onidajo99@gmail.com>
2024-05-29 07:46:07 +0000

    Edited bash script

--- finnhub-batch-stock-pipeline/apps/bash.sh
+++ finnhub-batch-stock-pipeline/apps/bash.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Function to detect the terminal emulator and open a new terminal
+open_new_terminal() {
+    local command="$1"
+    
+    # Check for common terminal emulators
+    if command -v gnome-terminal &> /dev/null; then
+        gnome-terminal -- bash -c "$command; exec bash"
+    elif command -v konsole &> /dev/null; then
+        konsole --noclose -e bash -c "$command; exec bash"
+    elif command -v xterm &> /dev/null; then
+        xterm -hold -e "$command; bash"
+    else
+        echo "No compatible terminal emulator found!"
+        exit 1
+    fi
+}
+
 SCRIPT_DIR=$(dirname "$BASH_SOURCE")
 
 wget -P $SCRIPT_DIR/ https://dl.min.io/server/minio/release/linux-amd64/minio
@@ -21,12 +38,19 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-
 
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-$SCRIPT_DIR/minio server ../data --console-address :9001
+minio_startcommand = "$SCRIPT_DIR/minio server ../data --console-address :9001"
+
+# xterm -hold -e "$minio_startcommand; bash"
+
+open_new_terminal "$minio_startcommand"
 $SCRIPT_DIR/mc alias set minio http://localhost:9000 minioadmin minioadmin
 $SCRIPT_DIR/mc admin info minio > minio.txt
 $SCRIPT_DIR/mc mb minio/dagster-api
 
-minikube start
+minikube_startcommand = "minikube start"
+
+open_new_terminal "minikube_startcommand"
+
 # sudo apt install apache2-utils
 
 # htpasswd -Bc finnhub-batch-stock-pipeline/apps/my-pdr/a2auth/registry.password dagster
